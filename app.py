d388d0c3db04b291448a138e37ec364db49822e0
wissyamore <wissalelamoury@gmail.com>
2024-09-06 19:59:59 +0800

    projects page

--- app.py
+++ app.py
@@ -10,7 +10,13 @@ def home():
 def exp():
     return render_template('experience.html')
 
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
 
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
